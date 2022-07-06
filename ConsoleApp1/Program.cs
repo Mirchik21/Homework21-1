10e8de8de3876bf519c6ff12af5eff178de666b1
Mirchik <kubanychbekovmirzaman@mail.ru>
2022-07-06 14:03:38 +0600

    initial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TV provaider = new TV(new string[] { "ALA-TV", "O-TV", "MegaLine-TV" }, "Dear subscriber, please choose a TV provider, which one do you prefer? \n ALA-TV, O-TV, MegaLine-TV");
+            TV channel = new TV(new string[] { "Sport", "KTRK", "Mir", "Music", "History" }, "Choose the channel you want to watch \n Sport, KTRK, Mir, Music, History");
+            ISwitchable[] channels = { provaider, channel };
+            foreach (TV tv in channels)
+            {
+                tv.SwitchTurnOn();
+            }
+            TV.PrintChannels(channels);
+
+            Console.WriteLine("If you want to turn off then press OFF");
+            string off = Console.ReadLine();
+            if (off == "OFF")
+            {
+                channel.SwitchTurnOff();
+            }
+            else
+            {
+                Console.WriteLine("press OFF");
+                off = Console.ReadLine();
+                channel.SwitchTurnOff();
+            }
+
+            Microwave microvolnovka = new Microwave();
+            microvolnovka.SwitchTurnOn();
+            microvolnovka.SwitchTurnOff();
+        }
+    }
+
+    class Microwave: ISwitchable
+    {
+       public void SwitchTurnOn()
+       {
+        Console.WriteLine("microwave is onn");
+       }
+
+        public void SwitchTurnOff()
+        {
+            Console.WriteLine("microwave is off");
+        }
+    }
+
+}
---
+++ ConsoleApp1/TV.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TV : ISwitchable
+    {
+        protected string name;
+        protected bool available = false;
+        string[] channelNames;
+        protected string orderQuestion;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public TV(string[] channelNames, string orderQuestion)
+        {
+            this.channelNames = channelNames;
+            this.orderQuestion = orderQuestion;
+        }
+        protected bool CheckAvailable()
+        {
+            return Array.Exists(channelNames, E => E == name);
+        }
+
+        public virtual void SwitchTurnOn()
+        {
+            while (!available)
+            {
+                Console.WriteLine(orderQuestion);
+                name = Console.ReadLine();
+                if (CheckAvailable())
+                {
+                    available = true;
+                }
+
+                if (available)
+                {
+                    Console.WriteLine("Ok, accepted.");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, choose something else");
+                }
+            }
+        }
+        public virtual void SwitchTurnOff()
+        {
+            Console.WriteLine("Thank you for choosing us, see you there!");
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public static void PrintChannels(ISwitchable[] channels)
+        {
+            
+            Console.WriteLine("You have selected a channel: ");
+            int counter = 1;
+            foreach (ISwitchable channel in channels)
+            {
+                Console.WriteLine($"{counter}) - {channel.ToString()}");
+                counter++;
+            }
+        }
+    }
+}
